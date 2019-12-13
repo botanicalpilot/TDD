8dad822a931feb4efac47030052d415aacbc7686
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-12-13 11:40:37 -0800

    refactored home page to use template

--- lists/tests.py
+++ lists/tests.py
@@ -1,26 +1,31 @@
+'''
+Unit tests are about testing logic, flow control, and configuration. Don't. Test. Constants.
+'''
+
+
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
 
+
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/') 
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_root_url_resolves_to_home_page_view(self):
-        #resolve is a Django function to resolve URLS and find what view f(x) they should map to. 
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_home_page_returns_correct_html(self):
+    #     #instead of manually creating an HttpResponse object can then calling view, 
+    #     # we can call self.client.get and pass the URL we want to test.
+    #     response = self.client.get('/')
 
-    def test_home_page_returns_correct_html(self):
-        #create a HttpRequest object which will be seen by Django when a user's browsers asks for homepage. 
-        request = HttpRequest()
-        #pass the request to the home_page, which gives response
-        response = home_page(request)
-        #extract the .content of the response in the form of raw bytes which must be decoded to UTF8
-        html = response.content.decode('utf8')
-        #make sure it starts with html tag
-        self.assertTrue(html.startswith('<html>'))
-        #we want a tag with To-Do lists in it somewhere in the html file. 
-        self.assertIn('<title>To-Do lists</title>', html)
-        #make sure it ends with html tag
-        self.assertTrue(html.endswith('</html>'))
+    #     html = response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+
+    #     #.assertTemplateUsed is the test method that Django TestCase class provides. 
+    #     # This method checks what template was used to render a response, 
+    #     # but will only work for responses that were retrieved by the test client. 
+    #     self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+'''
+Render takes the request as its first parameter and the name of the template to render. Django automatically looks for folders called 'templates' inside the apps dir and build an HttpResponse automatically so the programmer doesn't have to. 
+'''
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render (request, 'home.html')
