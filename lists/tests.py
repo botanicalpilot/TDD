4599e3a078bafab207755d15d66aa39d16723df4
Akins <Akins@Jamess-Air.localdomain>
2019-12-12 21:06:23 -0800

    basic view now returns minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -8,3 +10,17 @@ class HomePageTest(TestCase):
         #resolve is a Django function to resolve URLS and find what view f(x) they should map to. 
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        #create a HttpRequest object which will be seen by Django when a user's browsers asks for homepage. 
+        request = HttpRequest()
+        #pass the request to the home_page, which gives response
+        response = home_page(request)
+        #extract the .content of the response in the form of raw bytes which must be decoded to UTF8
+        html = response.content.decode('utf8')
+        #make sure it starts with html tag
+        self.assertTrue(html.startswith('<html>'))
+        #we want a tag with To-Do lists in it somewhere in the html file. 
+        self.assertIn('<title>To-Do lists</title>', html)
+        #make sure it ends with html tag
+        self.assertTrue(html.endswith('</html>'))
